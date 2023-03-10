20efd5bb31e3017cf6096512c3aa01564b4305fe
Brian Morrison II <brian@brianmorrison.me>
2023-03-10 11:13:12 -0500

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+command="pscale password create $1 $2 $3 --org $4 \
+  --service-token $PLANETSCALE_TOKEN --service-token-id $PLANETSCALE_TOKEN_NAME"
+
+if [ -n "$5" ];then
+  command="$command --role $5"
+fi
+
+CMDOUT=$(eval $command)
+
+username=$(jq -n "$CMDOUT" | jq '.username')
+password=$(jq -n "$CMDOUT" | jq '.plain_text')
+hostname=$(jq -n "$CMDOUT" | jq '.database_branch.access_host_url')
+echo "username=$username" >> $GITHUB_OUTPUT
+echo "password=$password" >> $GITHUB_OUTPUT
+echo "hostname=$hostname" >> $GITHUB_OUTPUT
